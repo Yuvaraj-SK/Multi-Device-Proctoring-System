c65b16673cef5ad23400c37d4ff137a614b6d201
Yuvaraj-SK <yuvarajkyatari411@gmail.com>
2026-07-26 00:16:32 +0530

    Setup Express server and MongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Proctoring Server API is Running...');
+// Test Route
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Backend Running 🚀"
+    });
 });
 
-module.exports = app;
+export default app;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
-const app = require('./app');
-require('dotenv').config();
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
 
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Start Express Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server Running on port ${PORT}...`);
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
