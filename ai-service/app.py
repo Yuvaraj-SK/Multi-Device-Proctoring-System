659a7d1f04c94a25de36094a6eb1bab474889186
MohanG805 <ggk0040@gmail.com>
2026-07-27 22:24:14 +0530

    Flask setup

---
+++ ai-service/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Home Route
+@app.route("/")
+def home():
+    return {
+        "success": True,
+        "message": "AI Service Running 🚀"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
